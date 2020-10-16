e6b19d769be2dbb003d780e8203de089145c4440
Abhishekh Dey <49533168+abhishekh-dey@users.noreply.github.com>
2020-10-16 16:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/* This site is developed by Abhishekh Dey (GDG Chandigarh) 
+ LinkedIn: https://www.linkedin.com/in/abhishekh-dey-19752738/ */ 
+
+$('.carousel').carousel({
+    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
+});
